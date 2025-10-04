9d8ffeae51a7854e923aa65fe6a9d5060ff195cf
baielldev <bajel8844@gmail.com>
2025-10-04 21:44:54 +0600

    Barbers Details done

--- src/app/barbers/page.tsx
+++ src/app/barbers/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import BannerBarbers from "./ui/barbersBanner";
 import ListBanners from "./ui/listBarbers";
 import InfoBranches from "./ui/infoBranches";
+import BarbersDetails from "./id/page";
+import Footer from "@/widgets/footer/ui";
 
 const Barbers = () => {
   return (
@@ -11,6 +13,13 @@ const Barbers = () => {
       <BannerBarbers />
       <ListBanners />
       <InfoBranches />
+      <BarbersDetails />
+      <div className="relative bg-[url(/footer-bg.jpg)] bg-left bg-cover w-full">
+        <div className="bg-black/50 absolute inset-0 z-0"></div>
+        <div className="z-10 pb-5 relative">
+          <Footer />
+        </div>
+      </div>
     </section>
   );
 };
--- src/app/barbers/ui/barbersBanner/index.tsx
+++ src/app/barbers/ui/barbersBanner/index.tsx
@@ -1,12 +1,29 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 import { barbersBannerData } from "./model";
 
 const BannerBarbers = () => {
   return (
-    <div className="h-[100vh] relative bg-[url(/barbers-bg.svg)] bg-cover bg-center max-lg:h-[80vh] max-md:h-[60vh] max-sm:h-[40vh]">
+    <motion.div
+      className="h-[100vh] relative bg-[url(/barbers-bg.svg)] bg-cover bg-center max-lg:h-[80vh] max-md:h-[60vh] max-sm:h-[40vh]"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <div className="absolute inset-0 bg-black/60"></div>
-      <div className="relative max-w-[1240px] mx-auto px-5 py-12">
-        <div className="text-[#FFF] pt-36 flex flex-col gap-8 max-lg:pt-18 max-lg:gap-4 max-md:pt-18 max-md:gap-5 max-sm:gap-2 max-sm:pt-10">
+      <motion.div
+        className="relative max-w-[1240px] mx-auto px-5 py-12"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+      >
+        <motion.div
+          className="text-[#FFF] pt-36 flex flex-col gap-8 max-lg:pt-18 max-lg:gap-4 max-md:pt-18 max-md:gap-5 max-sm:gap-2 max-sm:pt-10"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+        >
           <h1 className="text-[64px] font-bold max-md:text-4xl max-sm:text-2xl">
             BARBERS OF <span className="text-[#E9C664]">KINGSMAN</span>
           </h1>
@@ -14,21 +31,32 @@ const BannerBarbers = () => {
             Professionals <br />
             with impeccable taste
           </p>
-        </div>
+        </motion.div>
+
         <div className="mt-16 grid grid-cols-3 gap-6 text-white max-lg:mt-10 max-lg:gap-2 max-md:gap-0 max-sm:mt-2">
-          {barbersBannerData.map((i) => (
-            <div key={i.id} className="flex flex-col items-center">
+          {barbersBannerData.map((i, index) => (
+            <motion.div
+              key={i.id}
+              className="flex flex-col items-center"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: index * 0.2,
+                duration: 0.8,
+                ease: "easeOut",
+              }}
+            >
               <span className="text-5xl font-bold text-[#E9C664] max-md:text-3xl max-sm:text-[22px]">
                 {i.number}+
               </span>
               <p className="text-lg max-md:text-[14px] max-sm:text-center max-sm:text-[12px]">
                 {i.text}
               </p>
-            </div>
+            </motion.div>
           ))}
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
